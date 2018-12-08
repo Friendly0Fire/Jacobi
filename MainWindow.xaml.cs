4524b74236207b0d0bd4a3d027f7b9207022c91a
Jean-Philippe Guertin <jpguertin.cg@gmail.com>
2018-12-08 00:14:22 -0500

    Playing with data bindings.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +22,34 @@ namespace Jacobi
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static readonly DependencyProperty RProperty = DependencyProperty.Register("R", typeof(int), typeof(MainWindow), new FrameworkPropertyMetadata(0, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+        public int R { get { return (int)GetValue(RProperty); } set { SetValue(RProperty, value); } }
+
         public MainWindow()
         {
+            DataContext = this;
+
             InitializeComponent();
+        }
+
+        private void Slider_MouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            //if(sender == SliderR || sender == IntegerR)
+            //    R += e.Delta;
+        }
+    }
+
+    public class IntToDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (double)(int)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return (int)(double)value;
         }
     }
 }
